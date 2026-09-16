94166f46752e0261aaa6635e92a3cc5dca91ed7b
Satwik Saxena <satwiksaxena41@gmail.com>
2026-09-16 23:08:43 +0530

    Update Contact.tsx

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -117,7 +117,7 @@ export default function Contact() {
       setLoading(true);
 
       const response = await fetch(
-        "https://email-server-1-e9ua.onrender.com//api/contact",
+        "https://email-server-1-e9ua.onrender.com/api/contact",
         {
           method: "POST",
           headers: {
